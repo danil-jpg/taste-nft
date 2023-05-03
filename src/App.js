423ac20634d8312fca4077f63024ab2cd752aa45
Danil P <danil.polovinkin.1994@gmail.com>
2023-05-03 14:09:57 +0300

    completed some of  form

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Header from "./components/Header";
 import ArtworkPage from "./components/pages/ArtworkPage";
 import CreatorPage from "./components/pages/CreatorPage";
 import ModalsTemplate from "./components/UI/modals/ModalsTemplate";
+import { IconRenderer } from "./components/utils/IconRenderer";
 
 function App() {
   const [modal, setModal] = useState(false);
   const [walletState, setWalletState] = useState(1);
   const [logedIn, setLogStatus] = useState(false);
+  const [artwork, setArtwork] = useState(true);
   const [status, setStatus] = useState("Initializing...");
 
   const Wallet1 = () => {
@@ -73,6 +75,35 @@ function App() {
     }
   };
 
+  const Step1 = () => {
+    return (
+      <div className="step1">
+        <p className="step__title">Upload the artwork you will be selling</p>
+        <div className="step__drop"></div>
+        <button className="common-btn step__btn">Next step</button>
+      </div>
+    );
+  };
+
+  const Step2 = () => {
+    return (
+      <div className="step2">
+        <p className="step__title">
+          Censor the public version of artwork, if it contains 18+ content
+        </p>
+        <div className="step__edit">
+          <img src={require("./assets/img/modals/Rectangle 39322.png")} alt="img" />
+        </div>
+        <div className="step__filters">
+          <IconRenderer id={"picture"} className={"picture"}></IconRenderer>
+          <IconRenderer id={"del"} className={"del"}></IconRenderer>
+          <IconRenderer id={"drop"} className={"drop"}></IconRenderer>
+        </div>
+        <button className="common-btn step__btn">Next step</button>
+      </div>
+    );
+  };
+
   return (
     <div id="app">
       <BrowserRouter>
@@ -83,7 +114,26 @@ function App() {
           className={"modal__wallet"}>
           {Wallet()}
         </ModalsTemplate>
-        <Header callback={setModal} logedIn={logedIn}></Header>
+        <ModalsTemplate
+          className={"addArt"}
+          titleBlock={<p className="addArt__title">Creating artwork</p>}
+          visible={artwork}
+          setVisible={setArtwork}>
+          <div className="art__head">
+            <div className="arts__progress">
+              <div className="art__progress"></div>
+              <div className="art__progress"></div>
+              <div className="art__progress"></div>
+            </div>
+
+            <p className="art__progress_text">Step {1} from 3</p>
+          </div>
+
+          <div className="art__body">
+            <Step2></Step2>
+          </div>
+        </ModalsTemplate>
+        <Header callback={setModal} logedIn={logedIn} setArt={setArtwork}></Header>
         <Routes>
           <Route index element={<Main></Main>}></Route>
           <Route path="artwork" element={<ArtworkPage></ArtworkPage>}></Route>
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { IconRenderer as Icon } from "./utils/IconRenderer.js";
 import { useNavigate } from "react-router";
 import ModalsTemplate from "./UI/modals/ModalsTemplate";
 
-const Header = ({ callback, logedIn }) => {
+const Header = ({ callback, logedIn, setArt }) => {
   const [input, setInput] = useState("");
   const [showMore, setShowMore] = useState(false);
   const navigation = useNavigate();
@@ -12,9 +12,15 @@ const Header = ({ callback, logedIn }) => {
   const logedInFoo = () => {
     return (
       <div className="header__logedIn-wr">
-        <button className="header__btn-add-art common-btn">+ Add artwork</button>
+        <button className="header__btn-add-art common-btn" onClick={() => setArt(true)}>
+          + Add artwork
+        </button>
         <div className={showMore ? "header__user-wr active" : "header__user-wr"}>
-          <img className="header__user-_avatar" src={require("../assets/img/avatar.png")} alt="img" />
+          <img
+            className="header__user-_avatar"
+            src={require("../assets/img/avatar.png")}
+            alt="img"
+          />
           <div className="header__user-info-wr">
             <p className="header__user-info_name">User Name</p>
             <div className="header__user-info_sub-wr">
@@ -26,7 +32,9 @@ const Header = ({ callback, logedIn }) => {
               </div>
             </div>
           </div>
-          <button className={"header__logedIn_show-more-btn"} onClick={() => setShowMore(!showMore)}>
+          <button
+            className={"header__logedIn_show-more-btn"}
+            onClick={() => setShowMore(!showMore)}>
             <Icon id={"showMore"} className={showMore ? "showMore active" : "showMore "}></Icon>
           </button>
 
@@ -49,7 +57,13 @@ const Header = ({ callback, logedIn }) => {
       <Icon id={"logo"} className="logo" onClick={() => navigation("")}></Icon>
       <div className="header__input-wr">
         <Icon id={"icon-search"} className="icon-search"></Icon>
-        <input type="text" placeholder="Search for ..." className="header__input" value={input} onChange={(e) => setInput(e.target.value)} />
+        <input
+          type="text"
+          placeholder="Search for ..."
+          className="header__input"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
       </div>
       {logedIn ? (
         logedInFoo()
